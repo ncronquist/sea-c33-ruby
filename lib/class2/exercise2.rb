b7457bfd37ac8778826df8ecd8187ebe477fbafd
ncronquist <nick@ncronquist.com>
2015-02-18 20:49:29 -0800

    Complete class2 exercise2

--- lib/class2/exercise2.rb
+++ lib/class2/exercise2.rb
@@ -3,3 +3,8 @@
 # Assuming leap-years don't exist, display the minutes in a decade.
 #
 # TIP: I expect you to use a mathematical equation.
+#
+# 1 decade * 10 years/decade * 365 days/year * 24 hours/day * 60 minutes/hour
+
+minutes = 1 * 10 * 365 * 24 * 60
+puts minutes
