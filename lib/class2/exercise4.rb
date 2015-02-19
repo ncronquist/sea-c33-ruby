56c3087403f50abc9c286c24a74830b7e59808f1
ncronquist <nick@ncronquist.com>
2015-02-18 20:57:42 -0800

    Complete class2 exercise4

--- lib/class2/exercise4.rb
+++ lib/class2/exercise4.rb
@@ -13,3 +13,18 @@
 #   What's your last name?
 #   Jackson
 #   Nice to meet you, Samuel Leroy Jackson.
+
+# Ask user for their first name and then set a variable equal to their input
+puts "What's your first name?"
+first_name = gets.chomp.capitalize
+
+# Ask user for their middle name and then set a variable equal to their input
+puts "What's your middle name?"
+middle_name = gets.chomp.capitalize
+
+# Ask user for their last name and then set a variable equal to their input
+puts "What's your last name?"
+last_name = gets.chomp.capitalize
+
+# Print out a greeting with the person's full name.
+puts "Nice to meet you, #{first_name} #{middle_name} #{last_name}."
