efff3529afe5ebc83d22a5d5ea22e87359a3ae43
ncronquist <nick@ncronquist.com>
2015-02-24 11:29:13 -0800

    Add class3 exercise1

--- lib/class3/exercise1.rb
+++ lib/class3/exercise1.rb
@@ -16,3 +16,33 @@
 #   Go to the store and buy some more, 3 bottles of beer on the wall!
 #
 # TIP: I expect you to use a loop.
+
+# Methods
+def bottle_string(n)
+  if n > 1
+    return n.to_s + ' bottles of beer'
+  elsif n == 1
+    return n.to_s + ' bottle of beer'
+  elsif n == 0
+    return 'no more bottles of beer'
+  else
+    return 'ERROR! Can\'t have negative bottles of beer!'
+  end
+end
+
+# Begin Program
+count_start = 3
+count = count_start
+wall = 'on the wall'
+
+loop do
+  puts "#{bottle_string(count).capitalize} #{wall}, #{bottle_string(count)}!"
+
+  count -= 1
+  break if count == -1
+
+  puts "Take one down, pass it around, #{bottle_string(count)} #{wall}!"
+end
+
+# If we're out of beer get some more
+puts "Go to the store and buy some more, #{bottle_string(count_start)} #{wall}!"
