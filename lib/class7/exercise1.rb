c9856fcb8083cd8c26177990fac308161697e569
ncronquist <nick@ncronquist.com>
2015-03-08 15:06:56 -0700

    Add class7 exercise1

--- lib/class7/exercise1.rb
+++ lib/class7/exercise1.rb
@@ -47,29 +47,63 @@
 
 class Integer
   def hours_in_seconds
-    # replace me
+    self * 60 * 60
   end
 end
 
 class String
   def indent(amount = 2)
-    amount # replace me
+    space = ''
+    amount.times { space += ' ' }
+    space + self # replace me
   end
 end
 
 class Integer
   # rubocop:disable MethodLength
   def to_roman
-    # replace me
+    roman = ''
+    num = self
+
+    map = { 1000 => 'M',
+            900 => 'CM',
+            500 => 'D',
+            400 => 'CD',
+            100 => 'C',
+            90 => 'XC',
+            50 => 'L',
+            40 => 'XL',
+            10 => 'X',
+            9 => 'IX',
+            5 => 'V',
+            4 => 'IV',
+            1 => 'I'
+          }
+
+    i = 0
+    map.each do |a, r|
+      result = num / a
+      result.times do
+        roman += r
+      end
+      num = num % a
+      i += 1
+    end
+
+    roman
   end
 end
 
 class Array
   def second
-    # replace me
+    self[1]
   end
 
   def third
-    # replace me
+    self[2]
   end
 end
+
+puts 'foo'.indent
+
+puts [1, 2, 3, 4].third
